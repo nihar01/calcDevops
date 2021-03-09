935c3b598219e3794fce378d6bf20286a19109ed
nihar_kanwar@yahoo.co.in <nihar01>
2021-03-09 20:21:38 +0530

    Initial commit

---
+++ src/main/java/Calculator/Calculator.java
@@ -0,0 +1,76 @@
+package Calculator;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import java.util.Scanner;
+
+public class Calculator {
+    private static final Logger logger = LogManager.getLogger(Calculator.class);
+    public Calculator(){
+    }
+        public static void main(String args[])
+        {
+            int choice, flag=0;
+            double num1,num2;
+            Scanner scan = new Scanner(System.in);
+            System.out.println("Calculator");
+            do {
+                System.out.println("1 for Square Root\n2 for Factorial\n3 for Natural Logarithm\n4 for Power Function\n");
+                System.out.print("Enter your choice: ");
+                choice = scan.nextInt();
+                if (choice > 4 || choice <1) {
+                    flag = 1;
+                    System.out.println("Invalid choice, exiting\n");
+                }
+                else if (flag != 1){
+                    System.out.println("Enter the number");
+                    System.out.print("Enter number x: ");
+                    num1 = scan.nextDouble();
+//                    System.out.print("Enter number 2: ");
+//                    num2 = scan.nextDouble();
+
+                    switch(choice) {
+                        case 1: System.out.println("Square root of "+num1+" = "+ root(num1));
+                            break;
+                        case 2: System.out.println("Factorial of "+num1 +" = "+factorial(num1));
+                            break;
+                        case 3: System.out.println("Natural Log(base e) of "+num1+" = "+log(num1));
+                            break;
+                        case 4:
+                            System.out.println("Enter the power value: ");
+                            num2=scan.nextDouble();
+
+                            System.out.println("Power Function of"+ num1+"to power"+num2+" = "+power(num1,num2));
+                            break;
+                        default: System.out.println("Exiting, Bye");
+                            flag=1;
+                    }
+                }
+                System.out.println("\n");
+            }while(flag==0);
+        }
+
+       public static double root(double a) {
+           logger.info("[Square root] - " + a);
+        return Math.sqrt(a);
+    }
+      public  static double factorial(double a) {
+         if(a==0||a==1)
+             return a;
+         else
+         {
+             return a*factorial(a-1);
+         }
+    }
+      public  static double log(double a) {
+          logger.info("[log] - " + a);
+        return Math.log(a);
+    }
+       public static double power(double a, double b) {
+           logger.info("[power] - " + a);
+    return Math.pow(a,b);
+    }
+
+
+    }
+
---
+++ src/test/java/CalculatorTest.java
@@ -0,0 +1,67 @@
+import Calculator.Calculator;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+
+public class CalculatorTest {
+    private static final double DELTA = 1e-15;
+
+    Calculator calculator=new Calculator();
+
+    @Test
+    public void rootTruePositive()
+    {
+        assertEquals("Square root of number for True Positive", 10, calculator.root(100), DELTA);
+        assertEquals("Square root of number for True Positive", 5, calculator.root(25), DELTA);
+    }
+
+    @Test
+    public void rootFalsePositive()
+    {
+        assertNotEquals("Square root of number for False Positive", 12, calculator.root(121), DELTA);
+        assertNotEquals("Square root of number for False Positive", 4, calculator.root(17), DELTA);
+    }
+
+
+    @Test
+    public void factorialTruePositive()
+    {
+        assertEquals("Factorial of number for True Positive", 24, calculator.factorial(4), DELTA);
+        assertEquals("Factorial of number for True Positive", 120, calculator.factorial(5), DELTA);
+    }
+    @Test
+    public void factorialFalsePositive()
+    {
+        assertNotEquals("Factorial of number for False Positive", 125, calculator.factorial(5), DELTA);
+        assertNotEquals("Factorial of number for False Positive", 40, calculator.factorial(4), DELTA);
+    }
+
+    @Test
+    public void logTruePositive()
+    {
+        assertEquals("Natural Log of number for True Positive", 3.912023005428146, calculator.log(50), DELTA);
+        assertEquals("Natural Log of number for True Positive", 4.499809670330265, calculator.log(90), DELTA);
+    }
+
+    @Test
+    public void logFalsePositive()
+    {
+        assertNotEquals("Natural Log for False Positive", 12, calculator.log(121), DELTA);
+        assertNotEquals("Natural Log for False Positive", 4, calculator.log(17), DELTA);
+    }
+    @Test
+    public void powerTruePositive()
+    {
+assertEquals("power of number for true positive",729,calculator.power(9,3),DELTA);
+assertEquals("power of number for true positive",625,calculator.power(5,4),DELTA);
+    }
+    @Test
+    public void powerFalsePositive()
+    {
+        assertNotEquals("power of number for False Positive", 900, calculator.power(9,3), DELTA);
+        assertNotEquals("power of number for False Positive", 120, calculator.power(5,3), DELTA);
+    }
+    }
+
+
